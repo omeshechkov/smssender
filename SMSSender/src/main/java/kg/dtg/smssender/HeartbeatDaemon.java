2871f5fde49214edae7b2f59b962fc1bdfb5fa3f
Oleg Meshechkov <omeshechkov@gmail.com>
2014-01-24 14:17:27 +0600

    HeartbeatDaemon refactored

--- SMSSender/src/main/java/kg/dtg/smssender/HeartbeatDaemon.java
+++ SMSSender/src/main/java/kg/dtg/smssender/HeartbeatDaemon.java
@@ -26,34 +26,50 @@ public class HeartbeatDaemon extends Dispatcher {
     nodeName = properties.getProperty("node.name");
     interval = Long.parseLong(properties.getProperty("heartbeat"));
 
+    try (Connection connection = ConnectionAllocator.getConnection()) {
+
+      final PreparedStatement updateStatement = connection.prepareStatement(
+              "update `daemon-status` ds set ds.daemon = ?, ds.heartbeat = ? where ds.id = ?"
+      );
+      updateStatement.setString(1, "SMSSenderHeartbeat");
+      updateStatement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+      updateStatement.setString(3, nodeName);
+
+      if (updateStatement.executeUpdate() == 0) {
+        final PreparedStatement insertStatement = connection.prepareStatement(
+                "insert into `daemon-status` (id, daemon, heartbeat) values (?, ?, ?)"
+        );
+        insertStatement.setString(1, nodeName);
+        insertStatement.setString(2, "SMSSenderHeartbeat");
+        insertStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+
+        insertStatement.execute();
+      }
+
+      connection.commit();
+    } catch (Exception e) {
+      LOGGER.error("Cannot update heartbeat", e);
+    }
+
     new HeartbeatDaemon();
   }
 
   @Override
   protected void work() throws InterruptedException {
-    Connection connection = null;
-    try {
-      connection = ConnectionAllocator.getConnection();
-      
+    try (Connection connection = ConnectionAllocator.getConnection()) {
+
       final PreparedStatement statement = connection.prepareStatement(
-              "replace into `daemon-status` (id, daemon, heartbeat) values (?, ?, ?)"
+              "update `daemon-status` ds set ds.daemon = ?, ds.heartbeat = ? where ds.id = ?"
       );
-      statement.setString(1, nodeName);
-      statement.setString(2, "SMSSenderHeartbeat");
-      statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+      statement.setString(1, "SMSSenderHeartbeat");
+      statement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+      statement.setString(3, nodeName);
 
       statement.executeUpdate();
 
       connection.commit();
     } catch (Exception e) {
       LOGGER.error("Cannot update heartbeat", e);
-    } finally {
-      if (connection != null) {
-        try {
-          connection.close();
-        } catch (Exception ignored) {
-        }
-      }
     }
 
     Thread.sleep(interval);
